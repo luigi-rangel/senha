ed3a953da4e9facf5f4e1a6e210ec63a06fa9a23
luigi-rangel <luigimsr@dcc.ufrj.br>
2022-01-29 19:07:27 -0300

    mais letreco

--- senha.c
+++ senha.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<time.h>
 #include<stdbool.h>
 
@@ -18,49 +19,54 @@ void encontrarPalavra(char *palavra){
     return;
 }
 
-int letraNaoChecada(char *checagem, char letra, int *qtd){
-    for(int i = 0; i < MAX_LETRAS + 1; i++){
-        if(letra == checagem[i]) return 0;
-    }
-    *qtd += 1;
-    return 1;
+void printVerde(char string){
+    printf("\033[0;32m");
+    printf("%c", string);
+    printf("\033[0m");
 }
 
-bool exibirAcertos(char *gabarito, char *chute, int i){
-    char acertos[5] = {0, 0, 0, 0, 0};
-    int qtd = 0;
+void printAmarelo(char string){
+    printf("\033[0;33m");
+    printf("%c", string);
+    printf("\033[0m");
+}
 
+bool exibirAcertos(char *gabarito, char *chute, int k){
+    int acertos = 0;
+    
+    char gabaritoCopia[6];
+    strcpy(gabaritoCopia, gabarito);
+    
     for(int i = 0; i < 5; i++){
-        if(gabarito[i] == chute[i]){
-            printf("%c", gabarito[i]);
-            acertos[qtd] = gabarito[i];
-            qtd++;
-        }
-        else printf("*");
-    }
+        bool correto = false;
 
-    if(qtd == 5){
-        printf("\n\n");
-        return true;
-    }
-
-    printf(" - ");
-
-    for(int i = 0; i < 5; i++){
         for(int j = 0; j < 5; j++){
-            if(chute[i] == gabarito[j] && letraNaoChecada(acertos, gabarito[j], &qtd)){
-                printf("%c ", chute[i]);
-                acertos[qtd] = gabarito[j];
+            if(chute[i] == gabaritoCopia[j]){
+                correto = true;
+                acertos++;
+
+                if(i == j){
+                    gabaritoCopia[j] = '\0';
+                    printVerde(chute[i]);
+                }
+                else{
+                    printAmarelo(chute[i]);
+                }
+
+                break;
             }
         }
+
+        if(!correto){
+            printf("%c", chute[i]);
+        }
     }
 
-    if (i == 5){
-        printf("\n\n");
-        return false;
-    }
+    if(acertos == 5) return true;
 
-    printf("\n%d\n", i + 2);
+    printf("\n\n%d\n", k + 2);
+
+    return false;
 }
 
 int main(void){
@@ -70,11 +76,11 @@ int main(void){
     
     bool ganhou = false;
 
-    for(int i = 0; i < 6 && ganhou == false; i++){
+    for(int k = 0; k < 6 && ganhou == false; k++){
         char entrada[6];
         fgets(entrada, 6, stdin);
         while(getchar() != '\n');
-        ganhou = exibirAcertos(palavra, entrada, i);
+        ganhou = exibirAcertos(palavra, entrada, k);
     }
 
     if (ganhou == false){
